216a0c4d381d0459d0feb5f8d68a44d9320a5d4d
LapshinIV <ivan.lapshin@39parrot.net>
2016-06-27 00:05:16 +0400

    add some functional

---
+++ db.js
@@ -0,0 +1,70 @@
+var listOfFlights = {
+	"AB1322":{
+		flightNumber: "AB1322", 
+		passengers: [ 
+				{
+					"name":     "Dmitry Kouznetsov",
+					"booking":  "ABCDEF"
+				},
+				{
+					"name":     "Natalia Shteinikova", 
+					"booking":  "ABCDEF"
+				}, 
+		] 
+	}, 
+	"CD224":{ 
+		flightNumber: "CD224",
+		passengers: [ 
+				{
+					"name":     "Alex Emshanov",
+					"booking":  "ADADADAD"
+				},
+				{
+					"name":     "Alexandra Emshanova",
+					"booking":  "ADADADAD"
+				},
+			 {
+					"name":     "Avrora Emshanova",
+					"booking":  "ADADADAD"
+				},
+		] 
+	}, 
+	"QR335":{ 
+		flightNumber: "QR335", 
+		passengers: [ 
+				{
+					"name":     "Ivan Lapshin",
+					"booking":  "QWERT"
+				},
+				{
+					"name":     "Macaw Macaw",
+					"booking":  "QWERT"
+				},
+				{
+					"name": "Macaw1 Macaw1",
+					"booking":  "QWERT"
+				},
+		] 
+	},
+	"BB554":{ 
+		flightNumber: "BB554", 
+		passengers: [ 
+				{
+					"name":     "Parrot",
+					"booking":  "TYY"
+				},
+				{
+					"name":     "Monkey",
+					"booking":  "TYY"
+				},
+				{
+					"name": "Piton",
+					"booking":  "TYY"
+				},
+				{
+					"name": "Elephan",
+					"booking":  "TYY"
+				},
+		] 
+	}  
+}
--- test.js
+++ test.js
@@ -1,79 +1,81 @@
-var ListOfFlights = [
-{
-	flightNumber: "AB1322", 
-	passengers: [ 
-			{
-				"name":     "Dmitry Kouznetsov",
-				"booking":  "ABCDEF"
-			},
-			{
-				"name":     "Natalia Shteinikova", 
-				"booking":  "ABCDEF"
-			}, 
-	] 
-}, 
-{ 
-	flightNumber: "CD224",
-	passengers: [ 
-			{
-				"name":     "Alex Emshanov",
-				"booking":  "ADADADAD"
-			},
-			{
-				"name":     "Alexandra Emshanova",
-				"booking":  "ADADADAD"
-			},
-		 {
-				"name":     "Avrora Emshanova",
-				"booking":  "ADADADAD"
-			},
-	] 
-}, 
-{ 
-	flightNumber: "QR335", 
-	passengers: [ 
-			{
-				"name":     "Ivan Lapshin",
-				"booking":  "QWERT"
-			},
-			{
-				"name":     "Macaw Macaw",
-				"booking":  "QWERT"
-			},
-			{
-				"name": "Macaw1 Macaw1",
-				"booking":  "QWERT"
-			},
-	] 
-},
-{ 
-	flightNumber: "BB554", 
-	passengers: [ 
-			{
-				"name":     "Parrot",
-				"booking":  "TYY"
-			},
-			{
-				"name":     "Monkey",
-				"booking":  "TYY"
-			},
-			{
-				"name": "Piton",
-				"booking":  "TYY"
-			},
-			{
-				"name": "Elephan",
-				"booking":  "TYY"
-			},
-	] 
-}  
-]
+var listOfFlights = {
+	"AB1322":{
+		flightNumber: "AB1322", 
+		passengers: [ 
+				{
+					"name":     "Dmitry Kouznetsov",
+					"booking":  "ABCDEF"
+				},
+				{
+					"name":     "Natalia Shteinikova", 
+					"booking":  "ABCDEF"
+				}, 
+		] 
+	}, 
+	"CD224":{ 
+		flightNumber: "CD224",
+		passengers: [ 
+				{
+					"name":     "Alex Emshanov",
+					"booking":  "ADADADAD"
+				},
+				{
+					"name":     "Alexandra Emshanova",
+					"booking":  "ADADADAD"
+				},
+			 {
+					"name":     "Avrora Emshanova",
+					"booking":  "ADADADAD"
+				},
+		] 
+	}, 
+	"QR335":{ 
+		flightNumber: "QR335", 
+		passengers: [ 
+				{
+					"name":     "Ivan Lapshin",
+					"booking":  "QWERT"
+				},
+				{
+					"name":     "Macaw Macaw",
+					"booking":  "QWERT"
+				},
+				{
+					"name": "Macaw1 Macaw1",
+					"booking":  "QWERT"
+				},
+		] 
+	},
+	"BB554":{ 
+		flightNumber: "BB554", 
+		passengers: [ 
+				{
+					"name":     "Parrot",
+					"booking":  "TYY"
+				},
+				{
+					"name":     "Monkey",
+					"booking":  "TYY"
+				},
+				{
+					"name": "Piton",
+					"booking":  "TYY"
+				},
+				{
+					"name": "Elephan",
+					"booking":  "TYY"
+				},
+		] 
+	}  
+}
+
+console.log(listOfFlights["AB1322"].passengers.name)
 
 
 
-ListOfFlights.map(function(el){
-	el.passengers.map(function(el1){
-		console.log(el1.name)
+// ListOfFlights.map(function(el){
+// 	el.passengers.map(function(el1){
+// 		console.log(el1.name)
 	
-	})
-})
+// 	})
+// })
